918fb1c93b4cc5b00b27c1ecec556d9b052a6cba
Dani Rodríguez <dani@danirod.es>
2023-11-10 20:54:30 +0100

    Add page metadata

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,31 @@
 import "@unocss/reset/tailwind-compat.css";
 import "~/assets/global.css";
 
-useHead({
+const meta = {
   title: "Dani Rodríguez, Freelance Web Developer",
+  description:
+    "I am a freelance software developer and this is my professional webpage. Which essentially means I can give you code in exchange for money.",
+  url: "https://danirod.dev",
+  icon: "/danirod_icon.png",
+};
+
+useHead({
+  title: meta.title,
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [{ rel: "icon", type: "image/png", href: meta.icon }],
+  meta: [
+    { name: "description", content: meta.description },
+    { name: "og:title", content: meta.title },
+    { name: "og:description", content: meta.description },
+    { name: "og:url", content: meta.url },
+    { name: "og:image", content: meta.icon },
+    { name: "twitter:title", content: meta.title },
+    { name: "twitter:description", content: meta.description },
+    { name: "twitter:image", content: meta.icon },
+    { name: "twitter:card", content: "summary" },
+  ],
   bodyAttrs: {
     class: "cursor-auto",
   },
