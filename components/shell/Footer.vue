d5f2b7f9cf23ff8cbcffbc3139f651537f1b03c0
Dani Rodríguez <dani@danirod.es>
2023-12-19 12:45:42 +0100

    Update footer

--- components/shell/Footer.vue
+++ components/shell/Footer.vue
@@ -1,19 +1,23 @@
 <template>
   <footer class="my-10 md:my-20 container">
     <hr />
-    <p class="font-size-copy lh-copy mb-4 md:mb-8">
+    <p class="font-size-copy lh-copy mb-4 md:mb-8 xl:w-70%">
       <strong class="font-600">{{ $t("footer.colophon") }}</strong
-      >: {{ $t("footer.powered_by") }}<br />
-      {{ $t("footer.wordpress") }}<br />
-      {{ $t("footer.do_not_print") }}
+      >: {{ $t("footer.powered_by") }}. {{ $t("footer.wordpress") }}.
+      {{ $t("footer.do_not_print") }}. {{ $t("footer.this_is_open_source") }}:
+      <a href="https://github.com/danirod/danirod.dev" target="_blank">{{
+        $t("footer.check_out_the_code")
+      }}</a>
     </p>
     <div
       class="opacity-90 font-size-copy lh-copy text-center flex flex-col md:text-start md:flex-row gap-5 md:flex-justify-between md:flex-items-end"
     >
       <p class="font-600">
-        Copyright © {{ new Date().getFullYear() }} Dani Rodríguez
+        {{ $t("footer.copyright", { year: new Date().getFullYear() }) }}
       </p>
-      <p><span class="madein">Made in Spain</span></p>
+      <p>
+        <span class="madein">{{ $t("footer.made_in_spain") }}</span>
+      </p>
     </div>
   </footer>
 </template>
